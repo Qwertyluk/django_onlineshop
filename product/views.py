00c52ea9f0bf10d238d1d4e7e3ce702b8d084fde
Qwertyluk <krzyzakpawel@op.pl>
2020-12-30 20:35:49 +0100

    Make the home and the detail product html files more readable

--- product/views.py
+++ product/views.py
@@ -4,6 +4,5 @@ from .models import Product
 # Create your views here.
 def productDetails(request, id):
     product = get_object_or_404(Product, id = id)
-    print(product.Name)
     context = {'product': product}
     return render(request, 'product/details.html', context)
